1458a6d593fedc87fffc31c8fe0339b7d85486ef
joshuaArthurGreig <jagreig15@gmail.com>
2022-02-09 10:53:34 -0800

    eh 2

--- app/src/main/java/com/example/vaccinationlistactivity/VaccinationListActivity.kt
+++ app/src/main/java/com/example/vaccinationlistactivity/VaccinationListActivity.kt
@@ -3,9 +3,13 @@ package com.example.vaccinationlistactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.vaccinationlistactivity.databinding.ActivityVaccinationListBinding
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class VaccinationListActivity : AppCompatActivity() {
 
@@ -22,7 +26,6 @@ class VaccinationListActivity : AppCompatActivity() {
         binding = ActivityVaccinationListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val vaccinationAPI = Retrofit
 
         val inputStream = resources.openRawResource(R.raw.data)
         val jsonString = inputStream.bufferedReader().use {
@@ -32,5 +35,27 @@ class VaccinationListActivity : AppCompatActivity() {
         val type = object : TypeToken<List<VaccinationInfo>>() { }.type
         val vaccineList = gson.fromJson<List<VaccinationInfo>>(jsonString, type)
         Log.d(TAG, "onCreate: \n$vaccineList")
+
+
+        val vaccineAPI = RetrofitHelper.getInstance().create(Covid19Service::class.java)
+        val vaccineCall = vaccineAPI.getVaccinations(10)
+
+        vaccineCall.enqueue(object: Callback<List<VaccinationInfo>>() {
+            override fun onResponse(
+                call: Call<List<VaccinationInfo>>,
+                response: Response<List<VaccinationInfo>>
+            ) {
+                Log.d(TAG, "onResponse: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<List<VaccinationInfo>>, t: Throwable) {
+                Log.d(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+
+        val adapter = VaccineAdapter(vaccineList)
+        binding.recyclerViewVaccinationList.adapter = adapter
+        binding.recyclerViewVaccinationList.layoutManager = LinearLayoutManager(this)
     }
 }
