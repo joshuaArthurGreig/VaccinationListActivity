91dd4a943d6618388f94a8f6d0e470c951b0eee8
joshuaArthurGreig <jagreig15@gmail.com>
2022-02-11 10:04:53 -0800

    almost eh

--- app/src/main/java/com/example/vaccinationlistactivity/VaccinationListActivity.kt
+++ app/src/main/java/com/example/vaccinationlistactivity/VaccinationListActivity.kt
@@ -3,6 +3,10 @@ package com.example.vaccinationlistactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.vaccinationlistactivity.databinding.ActivityVaccinationListBinding
 import com.google.gson.Gson
@@ -33,19 +37,31 @@ class VaccinationListActivity : AppCompatActivity() {
         }
         val gson = Gson()
         val type = object : TypeToken<List<VaccinationInfo>>() { }.type
-        val vaccineList = gson.fromJson<List<VaccinationInfo>>(jsonString, type)
+        var vaccineList = gson.fromJson<List<VaccinationInfo>>(jsonString, type)
         Log.d(TAG, "onCreate: \n$vaccineList")
 
 
         val vaccineAPI = RetrofitHelper.getInstance().create(Covid19Service::class.java)
         val vaccineCall = vaccineAPI.getVaccinations(10)
 
-        vaccineCall.enqueue(object: Callback<List<VaccinationInfo>>() {
+        vaccineCall.enqueue(object: Callback<List<VaccinationInfo>> {
             override fun onResponse(
                 call: Call<List<VaccinationInfo>>,
                 response: Response<List<VaccinationInfo>>
             ) {
                 Log.d(TAG, "onResponse: ${response.body()}")
+                vaccineList = response.body()
+                adapter = VaccineAdapter(vaccineList)
+                binding.recyclerViewVaccinationList.adapter = adapter
+                binding.recyclerViewVaccinationList.layoutManager = LinearLayoutManager(this@VaccinationListActivity)
+                val country1 = vaccineList[0]
+                val firstDay = country1.timeline.firstKey()
+                val lastDay = country1.timeline.lastKey()
+                country1.timeline.get(firstDay)
+
+                country1.timeline.toList().sortedBy {
+                    it.second
+                }[0]
             }
 
             override fun onFailure(call: Call<List<VaccinationInfo>>, t: Throwable) {
@@ -57,5 +73,43 @@ class VaccinationListActivity : AppCompatActivity() {
         val adapter = VaccineAdapter(vaccineList)
         binding.recyclerViewVaccinationList.adapter = adapter
         binding.recyclerViewVaccinationList.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.sortingmenu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.country -> {
+                Toast.makeText(this, "country", Toast.LENGTH_SHORT).show()
+                adapter.dataset = adapter.dataset.sortedBy { it.country }
+                adapter.notifyDataSetChanged()
+                true
+            }
+
+            R.id.increase -> {
+                Toast.makeText(this, "increase", Toast.LENGTH_SHORT).show()
+                adapter.dataset = adapter.dataset.sortedBy {
+                    (it.timeline.get(it.timeline.lastKey() ?: 0L))?.minus(
+                        (it.timeline.get(it.timeline.firstKey()) ?: 0L)
+                    )
+                }
+                adapter.notifyDataSetChanged()
+                true
+            }
+
+            R.id.vaccineNumber -> {
+                Toast.makeText(this, "VaccineNumber", Toast.LENGTH_SHORT).show()
+                adapter.dataset = adapter.dataset.sortedByDescending { it.timeline.get(it.timeline.lastKey()) }
+                adapter.notifyDataSetChanged()
+                true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+
     }
 }
